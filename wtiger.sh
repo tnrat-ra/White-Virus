ac8ce78109c4cfbd8be9113bcf1fcf659d243ce1
whitetigerz <50932142+whitetigerz@users.noreply.github.com>
2019-06-16 22:45:27 +0530

    Create wtiger.sh

---
+++ wtiger.sh
@@ -0,0 +1,11 @@
+apt update
+apt upgrade
+apt install git
+apt install figlet
+apt install ruby
+apt install php
+cd 
+cp -r white-tiger#.apk /sdcard
+clear
+figlet Prince Yasin |lolcat|lolcat
+echo -e "\e[1;34m[*] \e[32Virus Created successfully with name of white-tiger#.apk check your internal storage....\e[0m";

